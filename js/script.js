545ea3ac55e3f5a88bfdff8b21c835a07e46d20d
joaoKremerDev <joaokremer.dev@gmail.com>
2023-02-03 20:59:42 -0300

    typing function feita

---
+++ js/script.js
@@ -0,0 +1,6 @@
+let typed = new Typed(".typing", {
+    strings:["", "Desenvolvedor Web.", "Estudante de Engenharia de Software.", "Aluno da Origamid."],
+    typeSpeed: 40,
+    BackSpeed: 60,
+    loop: true
+});
--- js/style-switcher.js
+++ js/style-switcher.js
@@ -32,6 +32,12 @@ function setActiveStyle(color){
 
 
 const dayNight = document.querySelector('.day-night');
+dayNight.addEventListener('click', () => {
+    dayNight.querySelector('i').classList.toggle('fa-sun');
+    dayNight.querySelector('i').classList.toggle('fa-moon');
+    document.body.classList.toggle('dark');
+});
+
 window.addEventListener('load', () => {
     if(document.body.classList.contains("dark")){
         dayNight.querySelector('i').classList.remove('fa-moon')
@@ -40,4 +46,6 @@ window.addEventListener('load', () => {
     } else{
         dayNight.querySelector('i').classList.add('fa-moon');
     }
-})
+});
+
+
