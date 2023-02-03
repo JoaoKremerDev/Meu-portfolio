397883b0bdaf61a4290af0c283642ba837064c34
joaoKremerDev <joaokremer.dev@gmail.com>
2023-02-03 20:13:18 -0300

    dark-ligth e temas de cores feito

--- js/style-switcher.js
+++ js/style-switcher.js
@@ -1,4 +1,4 @@
-// toggle style switcher
+// toggle style switcher // Troca de evento do switcher
 
 const styleSwitcherToggle = document.querySelector('.style-switcher-toggler');
 styleSwitcherToggle.addEventListener('click', () =>{
@@ -6,9 +6,38 @@ styleSwitcherToggle.addEventListener('click', () =>{
     switcher.classList.toggle('open');
 });
 
+// Hide my style - switcher on scroll // Esconder o switcher 
 document.addEventListener("click", function(event) {
     const switcher = document.querySelector('.style-switcher');
     if (!switcher.contains(event.target) && switcher.classList.contains('open')) {
       switcher.classList.remove('open');
     }
   });
+
+// theme colors // tema de cores
+
+const alternateStyles = document.querySelectorAll('.alternate-style');
+function setActiveStyle(color){
+    alternateStyles.forEach( style => { 
+        if(color === style.getAttribute("title")){
+            style.removeAttribute("disabled");
+        } else {
+            style.setAttribute('disabled', 'true');
+        }
+    });
+};
+
+
+// theme light and dark mode // modo escuro e claro
+
+
+const dayNight = document.querySelector('.day-night');
+window.addEventListener('load', () => {
+    if(document.body.classList.contains("dark")){
+        dayNight.querySelector('i').classList.remove('fa-moon')
+        dayNight.querySelector("i").classList.add('fa-sun');
+        console.log('teste')
+    } else{
+        dayNight.querySelector('i').classList.add('fa-moon');
+    }
+})
