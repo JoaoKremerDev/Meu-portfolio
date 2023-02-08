3e64b4588f4cc886ce9fe8e2ba61fd63601ad831
joaoKremerDev <joaokremer.dev@gmail.com>
2023-02-08 12:23:19 -0300

    modifacoes JS-Menu-mobile

--- js/menu-mobile.js
+++ js/menu-mobile.js
@@ -5,10 +5,16 @@ function menuShow() {
   if (menuMobile.classList.contains('open')) {
     menuMobile.classList.remove('open');
     document.querySelector('.nav-toggler span').style.transform = 'rotate(0deg)';
-    document.querySelector('')
+    const sections = document.querySelectorAll('.section .section-title');
+    const ArraySections = Array.from(sections);
+    ArraySections.forEach( section => section.style.paddingTop ='0px');
   } else {
     menuMobile.classList.add('open');
     document.querySelector('.nav-toggler span').style.transform = 'rotate(180deg)';
+  
+    const sections = document.querySelectorAll('.section .section-title');
+    const ArraySections = Array.from(sections);
+    ArraySections.forEach( section => section.style.paddingTop ='120px');
   }
 }
 
@@ -18,5 +24,8 @@ document.addEventListener('click', function(event) {
   if (!menuMobile.contains(event.target) && !navToggler.contains(event.target)) {
     menuMobile.classList.remove('open');
     document.querySelector('.nav-toggler span').style.transform = 'rotate(0deg)';
+    const sections = document.querySelectorAll('.section .section-title');
+    const ArraySections = Array.from(sections);
+    ArraySections.forEach( section => section.style.paddingTop ='0px');
   }
 });
