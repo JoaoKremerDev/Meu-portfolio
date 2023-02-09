eead74b61d4b6366e75acc0609b4e9731020a523
joaoKremerDev <joaokremer.dev@gmail.com>
2023-02-09 12:19:28 -0300

    efeito accordion adicionado na section sobre

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 let typed = new Typed(".typing", {
-    strings:["", "", "Desenvolvedor Web.", "Estudante de Engenharia de Software.", "Aluno da Origamid."],
+    strings: ["", "", "Desenvolvedor Web.", "Estudante de Engenharia de Software.", "Aluno da Origamid."],
     typeSpeed: 40,
     BackSpeed: 60,
     loop: true
@@ -7,7 +7,7 @@ let typed = new Typed(".typing", {
 
 
 
-
+// ESSA PARTE DO CÓDIGO É PARA ADICIONAR CORES AO MENU DO LAYOUT PC
 const menuNavLinks = document.querySelectorAll('.nav li a');
 
 function navLinkToggle() {
@@ -22,3 +22,18 @@ function navLinkToggle() {
 }
 
 navLinkToggle();
+
+
+const accordionItems = document.querySelectorAll('.accordion .timeline-title')
+
+accordionItems.forEach(item => {
+    item.addEventListener('click', () => {
+        const title = item;
+        const text = title.nextElementSibling;
+        if (text.classList.contains('accordion-open')) {
+            text.classList.remove('accordion-open');
+        } else {
+            text.classList.add('accordion-open');
+        }
+    })
+})
